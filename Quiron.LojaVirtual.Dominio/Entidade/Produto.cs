f9094e6d361d22da75d8bb39efd54c01aeffaaf4
galante <fabiogalantemans@gmail.com>
2014-04-21 16:56:06 -0300

    Classe Produto

---
+++ Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Entidade
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string Categoria { get; set; }
+    }
+}
