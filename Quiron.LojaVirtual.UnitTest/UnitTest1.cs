8f8e3ad7275b855e998ecfc6c6a409be55198f15
galante <fabiogalantemans@gmail.com>
2014-05-26 20:59:32 -0300

    Classes de Paginacao

--- Quiron.LojaVirtual.UnitTest/UnitTest1.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTest1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiron.LojaVirtual.Web.HtmlHelpers;
+using Quiron.LojaVirtual.Web.Models;
 
 namespace Quiron.LojaVirtual.UnitTest
 {
@@ -7,9 +11,45 @@ namespace Quiron.LojaVirtual.UnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Take()
         {
+            //O operador Take é usado para selecionar os primeiros n objetos de uma coleção.
+
+            int[] numeros = {5,4,1,3,9,8,6,7,2,0};
+
+            var resultado = from num in numeros.Take(5) select num;
+
+             int[] teste = {5,4,1,3,9};
+
+             CollectionAssert.AreEqual(resultado.ToArray(), teste);
+
         }
 
+
+        [TestMethod]
+        public void Skip()
+        {
+
+            //O operador Skip ignora o(s) primeiro(s) n objetos de uma coleção.
+
+            int[] numeros = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            var resultado = from num in numeros.Take(5).Skip(2) select num;
+
+            int[] teste = { 1,3,9 };
+
+            CollectionAssert.AreEqual(resultado.ToArray(), teste);
+
+        }
+
+
+
+     
+
+
+       
+
+
+       
     }
 }
---
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+        public int ProdutosPorPagina = 8;
+        public ActionResult ListaProdutos(int pagina = 1)
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos
+                .OrderBy(p => p.Descricao)
+                .Skip((pagina - 1)*ProdutosPorPagina)
+                .Take(ProdutosPorPagina);
+
+
+
+            return View(produtos);
+        }
+    }
+}
