37310c080b13aabe79a8e851f960700defc2c808
galante <fabiogalantemans@gmail.com>
2014-09-14 12:23:12 -0300

    Carrinho ItemCarrinho

--- Quiron.LojaVirtual.Dominio/Entidades/Carrinho.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Carrinho.cs
@@ -1,21 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Quiron.LojaVirtual.Dominio.Entidades
 {
     public class Carrinho
     {
 
-        private readonly List<ItemCarrinho> _itensCarrinho = new List<ItemCarrinho>();
-    
-        
+        private readonly List<ItemCarrinho> _itemCarrinho = new List<ItemCarrinho>();
+
+        //Adicionar
+
         public void AdicionarItem(Produto produto, int quantidade)
         {
-            ItemCarrinho item = _itensCarrinho.FirstOrDefault(p => p.Produto.ProdutoId == produto.ProdutoId);
+            ItemCarrinho item = _itemCarrinho.FirstOrDefault(p => p.Produto.ProdutoId == produto.ProdutoId);
 
             if (item == null)
             {
-                _itensCarrinho.Add(new ItemCarrinho
+                _itemCarrinho.Add(new ItemCarrinho
                 {
                     Produto = produto,
                     Quantidade = quantidade
@@ -25,28 +29,50 @@ namespace Quiron.LojaVirtual.Dominio.Entidades
             {
                 item.Quantidade += quantidade;
             }
+
+
         }
 
 
-        public void RemoverItem(Produto produto)
+
+        // Remover item
+
+        public void RemevorItem(Produto produto)
         {
-            _itensCarrinho.RemoveAll(l => l.Produto.ProdutoId == produto.ProdutoId);
+            _itemCarrinho.RemoveAll(l => l.Produto.ProdutoId == produto.ProdutoId);
         }
+
+
+
+        //Obter o valor total
 
         public decimal ObterValorTotal()
         {
-            return _itensCarrinho.Sum(e => e.Produto.Preco*e.Quantidade);
+            return _itemCarrinho.Sum(e => e.Produto.Preco*e.Quantidade);
         }
 
-        public IEnumerable<ItemCarrinho> ItensCarrinhos
+        //Limpar carrinho
+
+        public void LimparCarrinho()
         {
-            get { return _itensCarrinho; }
+            _itemCarrinho.Clear();
         }
+
+        //Itens carrinho
+
+        public IEnumerable<ItemCarrinho> ItensCarrinho
+        {
+            get { return _itemCarrinho; }
+        }
+
+
     }
+
 
     public class ItemCarrinho
     {
         public Produto Produto { get; set; }
+
         public int Quantidade { get; set; }
     }
 }
---
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+
+        private readonly EfDbContext _context = new EfDbContext();
+
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _context.Produtos; }
+        }
+
+
+    }
+}
