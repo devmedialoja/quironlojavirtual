130bc7242ab24ef24200d56a5e195fab2c7f553e
galante <fabiogalantemans@gmail.com>
2014-10-29 13:00:56 -0200

    Fechar Pedido Post

--- Quiron.LojaVirtual.Web/Controllers/CarrinhoController.cs
+++ Quiron.LojaVirtual.Web/Controllers/CarrinhoController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Configuration;
+using System.Linq;
 using System.Web.Mvc;
 using Quiron.LojaVirtual.Dominio.Entidades;
 using Quiron.LojaVirtual.Dominio.Repositorio;
@@ -79,11 +80,41 @@ namespace Quiron.LojaVirtual.Web.Controllers
             return View(new Pedido());
         }
 
-      
 
-        
+        [HttpPost]
+        public ViewResult FecharPedido(Pedido pedido)
+        {
+            Carrinho carrinho = ObterCarrinho();
 
+            EmailConfiguracoes email = new EmailConfiguracoes
+            {
+                EscreverArquivo = bool.Parse(ConfigurationManager.AppSettings["Email.EscreverArquivo"] ?? "false")
+            };
 
+            EmailPedido emailPedido = new EmailPedido(email);
+
+            if (!carrinho.ItensCarrinho.Any())
+            {
+                ModelState.AddModelError("","Não foi possível concluir o pedido, seu carrinho está vazio!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                emailPedido.ProcessarPedido(carrinho, pedido);
+                carrinho.LimparCarrinho();
+                return View("PedidoConcluido");
+            }
+            else
+            {
+                return View(pedido);
+            }
+
+        }
+
+        public ViewResult PedidoConcluido()
+        {
+            return View();
+        }
      
     }
 }
