5427c6d4b1fe55cece0892c5b4d62b6c18ab0213
galante <fabiogalantemans@gmail.com>
2014-11-04 09:21:59 -0200

    Area Administriva

---
+++ Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Administrativo/Produto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
