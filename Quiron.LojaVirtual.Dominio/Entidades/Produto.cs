593e4f42a992fc66e1699c5b97adfa1b96272a62
galante <fabiogalantemans@gmail.com>
2014-11-25 08:04:52 -0200

    View Alterar

--- Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -1,9 +1,16 @@
-﻿namespace Quiron.LojaVirtual.Dominio.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Dominio.Entidades
 {
     public class Produto
     {
+
+        [HiddenInput(DisplayValue = false)]
         public int ProdutoId { get; set; }
         public string Nome { get; set; }
+
+        [DataType(DataType.MultilineText)]
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
         public string Categoria { get; set; }
--- Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
+++ Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Entidades;
 using Quiron.LojaVirtual.Dominio.Repositorio;
 
 namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
@@ -14,6 +16,15 @@ namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
             _repositorio = new ProdutosRepositorio();
             var produtos = _repositorio.Produtos;
             return View(produtos);
+        }
+
+
+        public ViewResult Alterar(int produtoId)
+        {
+            _repositorio = new ProdutosRepositorio();
+            Produto produto = _repositorio.Produtos
+                .FirstOrDefault(p => p.ProdutoId == produtoId);
+            return View(produto);
         }
     }
 }
