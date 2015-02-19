b755ade9831ae19d78177b520d204f3a1ede6153
devmedialoja <devmedia2014@outlook.com>
2015-02-19 09:03:29 -0200

    Aula 39

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/AdministradoresRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+
+  
+
+    public class AdministradoresRepositorio
+    {
+          private readonly EfDbContext _context = new EfDbContext();
+
+        public Administrador ObterAdministrador(Administrador administrador)
+        {
+            return _context.Administradores.FirstOrDefault(a => a.Login == administrador.Login);
+        }
+    }
+}
