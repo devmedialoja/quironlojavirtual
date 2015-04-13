089b2918d0d81b4e43a9ac1131aea648e7f75cab
devmedialoja <devmedia2014@outlook.com>
2015-04-13 08:46:23 -0300

    Aula 46

--- Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -32,4 +32,11 @@ namespace Quiron.LojaVirtual.Dominio.Entidades
 
 
     }
+
+
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string  SobreNome { get; set; }
+    }
 }
