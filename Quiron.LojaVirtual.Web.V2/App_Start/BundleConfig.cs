1f6f1974976a1f1fa14ee35162c23720895c2b54
devmedialoja <devmedia2014@outlook.com>
2015-08-24 07:22:06 -0300

    Aula 56

--- Quiron.LojaVirtual.Web.V2/App_Start/BundleConfig.cs
+++ Quiron.LojaVirtual.Web.V2/App_Start/BundleConfig.cs
@@ -8,24 +8,29 @@ namespace Quiron.LojaVirtual.Web.V2
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/css").Include(
+                      "~/css/*.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/js").Include(
+                     "~/js/jquery.js",
+                      "~/js/bootstrap.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            // <!-- SmartMenu http://www.smartmenus.org/ -->
+            bundles.Add(new StyleBundle("~/Content/startmenu").Include(
+                 "~/Content/sm-core-css.css",
+                  "~/Content/sm-simple/sm-simple.css"
+             ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/Scripts/startmenu").Include(
+                    "~/Scripts/jquery.smartmenus.min.js"));
+
+            //bundles.Add(new ScriptBundle("~/Scripts/jsprojetos").Include(
+            //        "~/Scripts/menu.js"));
+
+
+
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
